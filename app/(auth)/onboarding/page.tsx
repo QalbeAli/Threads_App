5ccb33939fcfed6fcbe6c8d8b9029bc5c7f890dd
MAC <mac@Qalb-E-Ali-Mac.local>
2023-08-29 22:13:47 +0500

    working on onboarding

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,7 +1,26 @@
+import AccountProfile from "@/components/forms/AccountProfile";
+import {currentUser} from "@clerk/nextjs"
+
 async function Page() {
+    const user = await currentUser();
+
+    const userInfo = {}
+
+    const userData = {
+        id:user?.id,
+        objctId:userInfo._id,
+        username:userInfo.username || user?.username,
+        name:userInfo?.name || user?.firstName || ""
+
+    }
+
     return (
-        <main>
-            <h1 className="head-tect">OnBoarding!!!</h1>
+        <main className="mx-auto d flex max-w-3xl flex-col justify-start px-10 py-20">
+            <h1 className="head-text">OnBoarding!!!</h1>
+            <p className="mt-3 text-base-regular text-light-2">Complete Your Profile Now To Use Threads</p>
+            <section className="mt-9 bg-dark-2 p-10">
+            <AccountProfile />
+            </section>
         </main>
     )
 }
--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,7 +1,7 @@
 import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
-
+import {dark} from "@clerk/themes"
 async function Topbar() {
     return(
         <nav className='topbar'>
@@ -28,6 +28,7 @@ async function Topbar() {
   
           <OrganizationSwitcher
             appearance={{
+              baseTheme:dark,
               elements: {
                 organizationSwitcherTrigger: "py-2 px-4",
               },
